f5315c7e820400e3685c49d8762a8c62d7548b9e
Henrique Rinaldi <henriquergr@hotmail.com>
2021-09-15 22:28:42 -0300

    refactor: finished structure and design

--- src/Pages/MySchedules/MySchedules.jsx
+++ src/Pages/MySchedules/MySchedules.jsx
@@ -23,13 +23,13 @@ const MySchedules = () => {
                 </header>
 
 
-                <div className="container-lg h-100">
-                    <h5 className='fw__medium'>Olá, Jéssica da Silva</h5>
+                <div className="container-lg d-flex flex-column col-md-6">
+                    <h5 className='fw__medium'>Olá, Jéssica da Silva!</h5>
                     <p className="fs-12 fw__light text-grey">veja aqui suas reservas no OrangeDesk.</p>
 
                     <p className='fw__light text-grey'>SÃO PAULO</p>
                     <div className="container-lg fw__bold">
-                        <div style={{ backgroundColor: '#FAFAFA' }} className="row align-items-center border rounded shadow mb-5">
+                        <div style={{ backgroundColor: '#FAFAFA' }} className="row align-items-center border rounded shadow mb-3">
                             <div className="col-10">
                                 <table className="table table-borderless">
                                     <thead>
@@ -46,12 +46,32 @@ const MySchedules = () => {
                                     </tbody>
                                 </table>
                             </div>
-                            <div className="col-2 ps-0"><FiXCircle cursor='pointer' size='2rem' color='#F20000' /></div>
+                            <div className="col-2 ps-0 text-center"><FiXCircle cursor='pointer' size='2rem' color='#F20000' /></div>
+                        </div>
+
+                        <div style={{ backgroundColor: '#FAFAFA' }} className="row align-items-center border rounded shadow mb-3">
+                            <div className="col-10">
+                                <table className="table table-borderless">
+                                    <thead>
+                                        <tr className='text-grey fw__light fs-10'>
+                                            <th className='pb-0 pt-4' scope="col" colspan='2'>data</th>
+                                            <th className='pb-0 pt-4' scope="col">mesa</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className='text-blue'>
+                                            <td>16/09/2021</td>
+                                            <td colspan='2'>30</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                            <div className="col-2 ps-0 text-center"><FiXCircle cursor='pointer' size='2rem' color='#F20000' /></div>
                         </div>
                     </div>
-                    <div className="row align-items-end">
-                        <button>Realizar novo agendamento</button>
-                    </div>
+                </div>
+                <div className="container col-md-6 align-self-center mt-auto mb-3">
+                    <button className='w-100 btn-orange'>Realizar novo agendamento</button>
                 </div>
 
             </div>
